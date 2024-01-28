2963e730d54d7b9feb349f6bb17e25c5ef1ce39b
sweety <157895347+erwsu@users.noreply.github.com>
2024-01-28 18:47:01 +0530

    Update autoenconder.py

--- autoenconder.py
+++ autoenconder.py
@@ -1,49 +1,55 @@
-import matplotlib.pyplot as plt 
-import tensorflow as tf 
-from tensorflow.keras import layers, losses 
-from tensorflow.keras.datasets import fashion_mnist 
-from tensorflow.keras.models import Model
-(x_train, _), (x_test, _) = fashion_mnist.load_data()
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-print (x_train.shape)
-print (x_test.shape)
-class Autoencoder(Model):
-    def _init_ (self, latent_dim, shape):
-        super(Autoencoder, self)._init_()
-        self.latent_dim = latent_dim
-        self.shape = shape
-        self.encoder = tf.keras.Sequential([
-         layers.Flatten(),
-         layers.Dense(latent_dim, activation= 'relu'),
-        ])
-        self.decoder = tf.keras.Sequential([
-            layers.Dense(tf.math.reduce_prod(shape),activation='sigmoid'),
-            layers.Reshape(shape)
-        ])
-    def call(self,x):
-      encoded=self.encoder(x)
-      decoded=self.decoder(encoded)
-      return decoded
-shape=x_test.shape[1:]
-latent_dim=64
-autoencoder=Autoencoder(latent_dim,shape)
-autoencoder.compile(optimizer='adam',loss=losses.MeanSquaredError())
-autoencoder.fit(x_train,x_train,epochs=10,shuffle=True,validation_data=(x_test,x_test))
-encoded_imgs=autoencoder.encoder(x_test).numpy()
-decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
-n=10
-plt.figure(figsize=(20,4))
-for i in range(n):
-  ax=plt.subplot(2,n,i+1)
-  plt.imshow(x_test[i])
-  plt.title("orginal")
+from keras.datasets import mnist
+from keras.layers import Input, Dense
+from keras.models import Model
+import numpy as np
+import matplotlib.pyplot as plt
+
+(X_train, _), (X_test, _) = mnist.load_data()
+X_train = X_train.astype('float32')/255
+X_test = X_test.astype('float32')/255
+
+X_train = X_train.reshape(len(X_train), np.prod(X_train.shape[1:]))
+
+X_test = X_test.reshape(len(X_test), np.prod(X_test.shape[1:]))
+
+print(X_train.shape)
+print(X_test.shape)
+
+input_img= Input(shape=(784,))
+
+encoded = Dense (units=32, activation='relu')(input_img)
+
+decoded = Dense(units=784, activation='sigmoid') (encoded)
+autoencoder = Model(input_img, decoded)
+encoder = Model(input_img, encoded) 
+autoencoder.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy'])
+autoencoder.fit(X_train, X_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(X_test, X_test))
+encoded_imgs = encoder.predict(X_test)
+predicted = autoencoder.predict(X_test)
+plt.figure(figsize=(40, 4))
+for i in range(10):
+  ax = plt.subplot(3, 20, i + 1)
+  plt.imshow(X_test[i].reshape(28, 28))
   plt.gray()
   ax.get_xaxis().set_visible(False)
   ax.get_yaxis().set_visible(False)
-  ax=plt.subplot(2,n,i+1+n)
-  plt.imshow(decoded_imgs[i])
-  plt.title("reconstructed")
+
+# display encoded image
+
+  ax = plt.subplot(3, 20, i + 1 + 20)
+  plt.imshow(encoded_imgs[i].reshape(8, 4))
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+
+# display reconstruction
+
+  ax = plt.subplot(3, 20, 2 * 20 + i + 1)
+  plt.imshow(predicted[i].reshape(28, 28))
   plt.gray()
   ax.get_xaxis().set_visible(False)
   ax.get_yaxis().set_visible(False)
